5be4b09cd53ead3bff38eb5e121c4e9c762ae49f
joemaffei <joemaffei@users.noreply.github.com>
2016-05-03 18:56:02 -0500

    Update gant.js

--- gant.js
+++ gant.js
@@ -1,6 +1,6 @@
 /*
 WARRIORJS SOLUTIONS
-translated to mostly-ES5 JavaScript by Joe Maffei
+translated to mostly-ES6 JavaScript by Joe Maffei
 from original Ruby code by Gant
 http://www.iconoclastlabs.com/blog/ruby-warrior-walkthrough-part-3
 
@@ -34,265 +34,273 @@ TOTAL POINTS: 575
 class Player {
 
   playTurn(w) {
-  	this.level9(w);
+    this.level9(w);
   }
 
   level1(w) {
-  	w.walk();
+    w.walk();
   }
 
   level2(w) {
-  	if (w.feel().isEmpty()) {
-  		w.walk();
-  	} else {
-  		w.attack();
-  	}
+    if (w.feel().isEmpty()) {
+      w.walk();
+    } else {
+      w.attack();
+    }
   }
 
   level3(w) {
-  	var restHealth = 7;
-	  if (w.feel().isEmpty()) {
-	  	if (w.health() < restHealth) {
-	  		w.rest();
-	  	} else {
-	  		w.walk();
-	  	}
-  	} else {
-  		w.attack();
-  	}
+    let restHealth = 7;
+    if (w.feel().isEmpty()) {
+      if (w.health() < restHealth) {
+        w.rest();
+      } else {
+        w.walk();
+      }
+    } else {
+      w.attack();
+    }
   }
 
-  level4(w) {  	
-  	var shouldRest = function(w) {
-  		var safe = (w.health() >= lastHealth);
-  		var badHealth = (w.health() < restHealth);
-  		return (safe && badHealth);
-  	};
+  level4(w) {
+    let shouldRest = function(w) {
+      let safe = (w.health() >= lastHealth);
+      let badHealth = (w.health() < restHealth);
+      return (safe && badHealth);
+    };
 
-	  if (w.feel().isEmpty()) {
-	  	if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk();
-	  	}
-  	} else {
-  		w.attack();
-  	}
+    if (w.feel().isEmpty()) {
+      if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk();
+      }
+    } else {
+      w.attack();
+    }
 
-  	lastHealth = w.health();
+    lastHealth = w.health();
   }
 
   level5(w) {
-  	var frontSpace = w.feel();
+    let frontSpace = w.feel();
 
-  	var shouldRest = function(w) {
-  		var safe = (w.health() >= lastHealth);
-  		var badHealth = (w.health() < restHealth);
-  		return (safe && badHealth);
-  	};
+    let shouldRest = function(w) {
+      let safe = (w.health() >= lastHealth);
+      let badHealth = (w.health() < restHealth);
+      return (safe && badHealth);
+    };
 
-	  if (frontSpace.isEmpty()) {
-	  	if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk();
-	  	}
-  	} else if (frontSpace.isCaptive()) {
-  		w.rescue();
-  	} else {
-  		w.attack();
-  	}
+    if (frontSpace.isEmpty()) {
+      if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk();
+      }
+    } else if (frontSpace.isCaptive()) {
+      w.rescue();
+    } else {
+      w.attack();
+    }
 
-  	lastHealth = w.health();
+    lastHealth = w.health();
   }
 
   level6(w) {
-  	var feelSpace = w.feel(direction);
+    let feelSpace = w.feel(direction);
 
-  	var safe = function(w) {
-  		return (w.health() >= lastHealth);
-  	};
+    let safe = function(w) {
+      return (w.health() >= lastHealth);
+    };
 
-  	var shouldRest = function(w) {
-  		var badHealth = (w.health() < restHealth);
-  		return (safe(w) && badHealth);
-  	};
+    let shouldRest = function(w) {
+      let badHealth = (w.health() < restHealth);
+      return (safe(w) && badHealth);
+    };
 
-  	var shouldFlee = function(w) {
-  		return (!safe(w) && (w.health() < fleeHealth));
-  	};
+    let shouldFlee = function(w) {
+      return (!safe(w) && (w.health() < fleeHealth));
+    };
 
-	  if (feelSpace.isEmpty()) {
-	  	if (shouldFlee(w)) {
-	  		direction = 'backward';
-	  		w.walk(direction);
-	  	} else if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk(direction);
-	  	}
-  	} else if (feelSpace.isCaptive()) {
-  		w.rescue(direction);
-  	} else if (feelSpace.isWall()) {
-  		direction = 'forward';
-  	} else {
-  		w.attack();
-  	}
+    if (feelSpace.isEmpty()) {
+      if (shouldFlee(w)) {
+        direction = 'backward';
+        w.walk(direction);
+      } else if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk(direction);
+      }
+    } else if (feelSpace.isCaptive()) {
+      w.rescue(direction);
+    } else if (feelSpace.isWall()) {
+      direction = 'forward';
+    } else {
+      w.attack();
+    }
 
-  	lastHealth = w.health();
+    lastHealth = w.health();
   }
 
   level7(w) {
-  	var feelSpace = w.feel(direction);
+    let feelSpace = w.feel(direction);
 
-  	var safe = function(w) {
-  		return (w.health() >= lastHealth);
-  	};
+    let safe = function(w) {
+      return (w.health() >= lastHealth);
+    };
 
-  	var shouldRest = function(w) {
-  		var badHealth = (w.health() < restHealth);
-  		return (safe(w) && badHealth);
-  	};
+    let shouldRest = function(w) {
+      let badHealth = (w.health() < restHealth);
+      return (safe(w) && badHealth);
+    };
 
-  	var shouldFlee = function(w) {
-  		return (!safe(w) && (w.health() < fleeHealth));
-  	};
+    let shouldFlee = function(w) {
+      return (!safe(w) && (w.health() < fleeHealth));
+    };
 
-	  if (feelSpace.isEmpty()) {
-	  	if (shouldFlee(w)) {
-	  		direction = 'backward';
-	  		w.walk(direction);
-	  	} else if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk(direction);
-	  	}
-  	} else if (feelSpace.isCaptive()) {
-  		w.rescue(direction);
-  	} else if (feelSpace.isWall()) {
-  		direction = 'forward';
-  		w.pivot();
-  	} else {
-  		w.attack();
-  	}
-  	lastHealth = w.health();
+    if (feelSpace.isEmpty()) {
+      if (shouldFlee(w)) {
+        direction = 'backward';
+        w.walk(direction);
+      } else if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk(direction);
+      }
+    } else if (feelSpace.isCaptive()) {
+      w.rescue(direction);
+    } else if (feelSpace.isWall()) {
+      direction = 'forward';
+      w.pivot();
+    } else {
+      w.attack();
+    }
+    lastHealth = w.health();
   }
 
   level8(w) {
-  	var feelSpace = w.feel(direction);
+    let feelSpace = w.feel(direction);
 
-  	var clearShot = function(w) {
-  		var glance = w.look(direction);
-  		var distanceToEnemy = function(glance) {
-	  		for (var i in glance) {
-	  			if (glance[i].isEnemy()) { return i; }
-	  		}
-	  		return maxGlance;
-	  	};
-	  	var distanceToCaptive = function(glance) {
-	  		for (var i in glance) {
-	  			if (glance[i].isCaptive()) { return i; }
-	  		}
-	  		return maxGlance;
-	  	};
-	  	return (distanceToEnemy(glance) < distanceToCaptive(glance));
-  	};
+    let clearShot = function(w) {
+      let glance = w.look(direction);
+      let distanceToEnemy = function(glance) {
+        for (let i in glance) {
+          if (glance[i].isEnemy()) {
+            return i;
+          }
+        }
+        return maxGlance;
+      };
+      let distanceToCaptive = function(glance) {
+        for (let i in glance) {
+          if (glance[i].isCaptive()) {
+            return i;
+          }
+        }
+        return maxGlance;
+      };
+      return (distanceToEnemy(glance) < distanceToCaptive(glance));
+    };
 
-  	var safe = function(w) {
-  		return (w.health() >= lastHealth);
-  	};
+    let safe = function(w) {
+      return (w.health() >= lastHealth);
+    };
 
-  	var shouldRest = function(w) {
-  		var badHealth = (w.health() < restHealth);
-  		return (safe(w) && badHealth);
-  	};
+    let shouldRest = function(w) {
+      var badHealth = (w.health() < restHealth);
+      return (safe(w) && badHealth);
+    };
 
-  	var shouldFlee = function(w) {
-  		return (!safe(w) && (w.health() < fleeHealth));
-  	};
+    let shouldFlee = function(w) {
+      return (!safe(w) && (w.health() < fleeHealth));
+    };
 
-  	if (clearShot(w)) {
-  		w.shoot();
-  	} else if (feelSpace.isEmpty()) {
-	  	if (shouldFlee(w)) {
-	  		direction = 'backward';
-	  		w.walk(direction);
-	  	} else if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk(direction);
-	  	}
-  	} else if (feelSpace.isCaptive()) {
-  		w.rescue(direction);
-  	} else if (feelSpace.isWall()) {
-  		direction = 'forward';
-  		w.pivot();
-  	} else {
-  		w.attack();
-  	}
-  	lastHealth = w.health();
+    if (clearShot(w)) {
+      w.shoot();
+    } else if (feelSpace.isEmpty()) {
+      if (shouldFlee(w)) {
+        direction = 'backward';
+        w.walk(direction);
+      } else if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk(direction);
+      }
+    } else if (feelSpace.isCaptive()) {
+      w.rescue(direction);
+    } else if (feelSpace.isWall()) {
+      direction = 'forward';
+      w.pivot();
+    } else {
+      w.attack();
+    }
+    lastHealth = w.health();
   }
 
   level9(w) {
-  	var feelSpace = w.feel(direction);
+    let feelSpace = w.feel(direction);
 
-  	var safe = function(w) {
-  		return (w.health() >= lastHealth);
-  	};
+    let safe = function(w) {
+      return (w.health() >= lastHealth);
+    };
 
-  	var clearShot = function(w) {
-  		var glance = w.look(direction);
-  		var distanceToEnemy = function(glance) {
-	  		for (var i in glance) {
-	  			if (glance[i].isEnemy()) { return i; }
-	  		}
-	  		return maxGlance;
-	  	};
-	  	var distanceToCaptive = function(glance) {
-	  		for (var i in glance) {
-	  			if (glance[i].isCaptive()) { return i; }
-	  		}
-	  		return maxGlance;
-	  	};
-	  	return ((safe(w)) && (distanceToEnemy(glance) < distanceToCaptive(glance)));
-  	};
+    let clearShot = function(w) {
+      let glance = w.look(direction);
+      let distanceToEnemy = function(glance) {
+        for (let i in glance) {
+          if (glance[i].isEnemy()) {
+            return i;
+          }
+        }
+        return maxGlance;
+      };
+      let distanceToCaptive = function(glance) {
+        for (let i in glance) { *
+          if (glance[i].isCaptive()) {
+            return i;
+          }
+        }
+        return maxGlance;
+      };
+      return ((safe(w)) && (distanceToEnemy(glance) < distanceToCaptive(glance)));
+    };
 
-  	var shouldRest = function(w) {
-  		var badHealth = (w.health() < restHealth);
-  		return (safe(w) && badHealth);
-  	};
+    let shouldRest = function(w) {
+      let badHealth = (w.health() < restHealth);
+      return (safe(w) && badHealth);
+    };
 
-  	var shouldFlee = function(w) {
-  		return (!safe(w) && (w.health() < fleeHealth));
-  	};
+    let shouldFlee = function(w) {
+      return (!safe(w) && (w.health() < fleeHealth));
+    };
 
-  	if (clearShot(w)) {
-  		w.shoot();
-  	} else if (feelSpace.isEmpty()) {
-	  	if (shouldFlee(w)) {
-	  		direction = 'backward';
-	  		w.walk(direction);
-	  	} else if (shouldRest(w)) {
-	  		w.rest();
-	  	} else {
-	  		w.walk(direction);
-	  	}
-  	} else if (feelSpace.isCaptive()) {
-  		w.rescue(direction);
-  	} else if (feelSpace.isWall()) {
-  		direction = 'forward';
-  		w.pivot();
-  	} else {
-  		w.attack();
-  	}
-  	lastHealth = w.health();
+    if (clearShot(w)) {
+      w.shoot();
+    } else if (feelSpace.isEmpty()) {
+      if (shouldFlee(w)) {
+        direction = 'backward';
+        w.walk(direction);
+      } else if (shouldRest(w)) {
+        w.rest();
+      } else {
+        w.walk(direction);
+      }
+    } else if (feelSpace.isCaptive()) {
+      w.rescue(direction);
+    } else if (feelSpace.isWall()) {
+      direction = 'forward';
+      w.pivot();
+    } else {
+      w.attack();
+    }
+    lastHealth = w.health();
   }
 }
 
-var restHealth = 15;
-var fleeHealth = 7;
-var lastHealth = 20;
-var maxGlance = 4;
-var direction = 'forward';
-
+let restHealth = 15,
+    fleeHealth = 7,
+    lastHealth = 20,
+    maxGlance = 4,
+    direction = 'forward';
+    
 global.Player = Player;
